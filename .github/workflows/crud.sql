6d81e15e73c8e886f12c59e85fefbfc39bb4f76e
mschroeder5 <48137324+mschroeder5@users.noreply.github.com>
2024-07-01 16:33:36 -0500

    Update crud.sql
    Inserted different values into database and updated code to include more CRUD operations.

--- .github/workflows/crud.sql
+++ .github/workflows/crud.sql
@@ -1,15 +1,24 @@
 --CRUD Operations
-INSERT INTO Products VALUES ("Townsend","Universal Audio","Microphone",1200); --C for Create
-SELECT DISTINCT Products.model,Products.manufacturer,Products.price FROM Products JOIN Microphones ON Products.model=Microphones.model WHERE mic_type="Dynamic"; --R for Read
+INSERT INTO Products VALUES ("SM57","Shure","Microphone",99); --C for Create
+--Adds Shure mic to database
+INSERT INTO Microphones VALUES ("SM57", "Dynamic", "Shure", "Cardioid", NULL, 99);
+
+SELECT Products.model, Products.manufacturer, Microphones.mic_type, Products.price
+FROM Products
+JOIN Microphones ON Products.model=Microphones.model
+WHERE mic_type="Dynamic"
+ORDER BY Products.price; --R for Read
 --Lists all microphones that are dynamic
 
-SELECT * FROM Products;
-UPDATE Products SET price=500 WHERE model="SM58"; --U for Update, updates SM58 model price to 500
-DELETE FROM Products WHERE model="Townsend"; --D for Delete, deletes rcord for microphone of the model "Townsend"
-SELECT * FROM Products;
+SELECT *
+FROM Products
+WHERE price<100; --Finds all products that cost less than $100
 
-SELECT * FROM Microphones;
-SELECT * FROM Cables;
-SELECT * FROM Interfaces;
-SELECT * FROM Mixers;
-SELECT * FROM Speakers;
+SELECT DISTINCT *
+FROM Products JOIN Microphones
+WHERE manufacturer = 'Shure';
+
+SELECT * FROM Products;
+UPDATE Products SET price=500 WHERE model="SM58"; --U for Update, updates SM58 mic's price to 500
+DELETE FROM Products WHERE model="SM57"; --D for Delete, deletes rcord for microphone of the model "Townsend"
+SELECT * FROM Products;
